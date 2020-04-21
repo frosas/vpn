f1e0790ca1248ab475cf4191da69d950cefe740c
Francesc Rosas <francescrosasbosque@gmail.com>
2020-04-21 11:21:40 +0100

    Linting

--- index.ts
+++ index.ts
@@ -3,17 +3,18 @@ import * as gcp from "@pulumi/gcp";
 const instance = new gcp.compute.Instance("vpn", {
   bootDisk: {
     initializeParams: {
-      image: "debian-10-buster-v20200413"
+      image: "debian-10-buster-v20200413",
+    },
   },
   machineType: "f1-micro",
   networkInterfaces: [
     {
       accessConfigs: [{}], // Enable external access
-      network: "default"
-    }
-  ]
+      network: "default",
+    },
+  ],
 });
 
 export const sshDestination = instance.networkInterfaces.apply(
-  nis => `frosas@${nis[0].accessConfigs![0].natIp}`
+  (nis) => `frosas@${nis[0].accessConfigs![0].natIp}`
 );
