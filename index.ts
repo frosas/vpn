adadfe7d729aa7d4e1525643ea92fca966a32e04
Francesc Rosas <francesc@francescrosas.com>
2025-01-03 17:27:07 +0100

    Local tunnel

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import * as gcp from "@pulumi/gcp";
 
-const instance = new gcp.compute.Instance("vpn", {
+const network = new gcp.compute.Network("network");
+
+new gcp.compute.Firewall("firewall", {
+  network: network.id,
+  allows: [{ protocol: "tcp", ports: ["0-65535"] }],
+  sourceRanges: ["0.0.0.0/0"],
+});
+
+const instance = new gcp.compute.Instance("instance", {
   bootDisk: {
     initializeParams: {
       image: "debian-12-bookworm-v20240415",
@@ -9,12 +17,21 @@ const instance = new gcp.compute.Instance("vpn", {
   machineType: "f1-micro",
   networkInterfaces: [
     {
-      accessConfigs: [{}], // Enable external access
-      network: "default",
+      network: network.id,
+      accessConfigs: [{}], // Allocates a public IP address
     },
   ],
+  metadataStartupScript: [
+    "echo AllowTcpForwarding yes >> /etc/ssh/sshd_config",
+    "echo GatewayPorts yes >> /etc/ssh/sshd_config",
+    "systemctl restart sshd",
+  ].join("\n"),
 });
 
-export const sshDestination = instance.networkInterfaces.apply(
-  (nis) => `frosas@${nis[0].accessConfigs![0].natIp}`
+export const instanceIp = instance.networkInterfaces.apply(
+  (nis) => nis[0].accessConfigs![0].natIp
+);
+
+export const sshDestination = instanceIp.apply(
+  (instanceIp) => `frosas@${instanceIp}`
 );
